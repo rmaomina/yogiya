eafae5847678ed87e4b59a929d1eefb7b4c416a7
Mina Kim <rmaomina@gmail.com>
2023-05-18 01:51:58 +0900

    [style] 레이아웃 초기 설정

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import Container from "../Container";
+
+const Navbar = () => {
+  return (
+    <div className="fixed z-10 w-full bg-white shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>anithing!!</Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
