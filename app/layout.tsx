458c6d96c9ae9714182d7eea2b9f3976672c16cf
Mina Kim <rmaomina@gmail.com>
2023-05-21 21:37:05 +0900

    [feat] 공통 모달 & 모달 버튼 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import "./globals.css";
 import { Nunito } from "next/font/google";
-import Navbar from "./components/Navbar/Navbar";
+// import Client from "./components/Client";
+import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
   title: "yogiya website",
-  description: "친구들과 가기 좋은 숙소 찾기 웹 사이트",
+  description: "친구들과 가기 좋은 숙소 찾기 요기야!",
 };
 
 export default function RootLayout({
@@ -17,6 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>
+        <Modal
+          actionLabel="Submit"
+          title="Common Modal"
+          isOpen
+          onSubmit={() => {}}
+          onClose={() => {}}
+        />
         <Navbar />
         {children}
       </body>
