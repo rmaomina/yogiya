d336d4f6c86281d7cc9f500f5420096e5780f781
Mina Kim <rmaomina@gmail.com>
2023-06-02 17:51:30 +0900

    [feat] 레이아웃 화면에서 콘텐츠 화면 분리

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,36 @@
-export default function Home() {
-  return <div className="text-2xl text-black">Loading...</div>;
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import getListing from "./actions/getListings";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
+
+export default async function Home() {
+  const listings = await getListing();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <Container>
+        <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard
+                key={listing.id}
+                data={listing}
+                currentUser={currentUser}
+              />
+            );
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
