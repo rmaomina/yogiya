51efce49391fe36167330120ead1f40decd0e6ae
Mina Kim <rmaomina@gmail.com>
2023-06-04 22:58:38 +0900

    [refactor] 포맷터 적용

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -26,7 +26,7 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
   return (
     <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden">
       <div className="flex flex-row items-center gap-1 p-4">
-        <div className="text-2xl font-semibold mr-1">$ {price}</div>
+        <div className="mr-1 text-2xl font-semibold">$ {price}</div>
         <div className="font-light text-neutral-600">night</div>
       </div>
       <hr />
@@ -43,7 +43,7 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
           onClick={onSubmit}
         />
       </div>
-      <div className="p-4 flex flex-row items-center justify-between font-semibold text-lg">
+      <div className="flex flex-row items-center justify-between p-4 text-lg font-semibold">
         <div>Total</div>
         <div>$ {totalPrice}</div>
       </div>
