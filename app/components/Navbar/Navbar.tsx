c1d0b800101cece1dcfc33ef43a949cb83b8e8fb
Mina Kim <rmaomina@gmail.com>
2023-06-06 03:50:25 +0900

    [fix] import 구분 중복 오류 수정

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 import Categories from "./Categories";
+import Container from "../Container";
 
 import { SafeUser } from "@/app/types";
 
